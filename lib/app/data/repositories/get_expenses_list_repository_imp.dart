7defee7525082428f97466537be116d7dec760d2
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-20 00:18:17 -0300

    feat: :sparkles: implementação da listagem de gastos do repositório

--- lib/app/data/repositories/get_expenses_list_repository_imp.dart
+++ lib/app/data/repositories/get_expenses_list_repository_imp.dart
@@ -1,11 +1,14 @@
+import 'package:my_expenses/app/data/data_sources/get_expenses_list_data_source.dart';
 import 'package:my_expenses/app/domain/entities/expense_entity.dart';
 import 'package:my_expenses/app/domain/repositories/get_expenses_list_repository.dart';
 
 class GetExpensesListRepositoryImp extends GetExpensesListRepository{
+  final GetExpensesListDataSource _getExpensesListDataSource;
+
+  GetExpensesListRepositoryImp(this._getExpensesListDataSource);
   @override
-  Future<List<ExpenseEntity>> call() {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<List<ExpenseEntity>> call() async {
+   return await _getExpensesListDataSource();
   }
 
 }
