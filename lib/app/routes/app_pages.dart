d4ee549e123207a80871d3646e8edbc1c074b787
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-20 23:14:48 -0300

    refactor: :truck: movendo para um novo diretorio

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,14 +1,14 @@
 import 'package:get/get.dart';
 
-import '../modules/home/bindings/home_binding.dart';
-import '../modules/home/views/home_view.dart';
+import '../presentation/modules/home/bindings/home_binding.dart';
+import '../presentation/modules/home/views/home_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const initial = Routes.HOME;
 
   static final routes = [
     GetPage(
