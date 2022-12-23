0774f32eba875d14995857f9d3d8b606139f42c2
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-23 14:04:02 -0300

    fix: :bug: Implementando a classe correta.

--- lib/app/data/data_sources/local/get_planned_expenses_list_local_data_source_imp.dart
+++ lib/app/data/data_sources/local/get_planned_expenses_list_local_data_source_imp.dart
@@ -3,19 +3,21 @@ import 'package:my_expenses/app/data/data_sources/get_planned_expenses_list_data
 import 'package:my_expenses/app/data/dtos/planned_expenses_dto.dart';
 import 'package:my_expenses/app/domain/entities/planned_expenses_entity.dart';
 
-class GetPlannedExpensesListLocalDataSourceImp implements GetPlannedExpensesListDataSource{
+class GetPlannedExpensesListLocalDataSourceImp
+    implements GetPlannedExpensesListDataSource {
   final GetDataBaseSQLiteImp _getDataBaseImp;
 
   GetPlannedExpensesListLocalDataSourceImp(this._getDataBaseImp);
   @override
-  Future<List<PlannedExpensesEntity>> call() async{
+  Future<List<PlannedExpensesEntity>> call() async {
     final databaseResponse = await _getDataBaseImp();
-    List<Map<String,dynamic>> queryResponse = await databaseResponse.query('planned_expenses');
-    List<PlannedExpensesDTO> plannedExpensesList = List.generate(queryResponse.length, (index) {
+    List<Map<String, dynamic>> queryResponse =
+        await databaseResponse.query('planned_expenses');
+    List<PlannedExpensesDTO> plannedExpensesList =
+        List.generate(queryResponse.length, (index) {
       var element = queryResponse[index];
       return PlannedExpensesDTO.fromMapLocalDatabase(element);
     });
     return plannedExpensesList;
   }
-
 }
--- lib/app/data/repositories/get_planned_expenses_repository_imp.dart
+++ lib/app/data/repositories/get_planned_expenses_repository_imp.dart
@@ -1,13 +1,14 @@
-import 'package:my_expenses/app/data/data_sources/get_planned_expenses_List_data_source.dart';
+import 'package:my_expenses/app/data/data_sources/get_planned_expenses_list_data_source.dart';
 import 'package:my_expenses/app/domain/entities/planned_expenses_entity.dart';
 import 'package:my_expenses/app/domain/repositories/get_planned_expenses_list_repository.dart';
 
-class GetPlannedExpensesListRepositoryImp implements GetPlannedExpensesListRepository{
+class GetPlannedExpensesListRepositoryImp
+    implements GetPlannedExpensesListRepository {
   final GetPlannedExpensesListDataSource _getPlannedExpensesListDataSource;
 
   GetPlannedExpensesListRepositoryImp(this._getPlannedExpensesListDataSource);
   @override
-  Future<List<PlannedExpensesEntity>> call() async{
+  Future<List<PlannedExpensesEntity>> call() async {
     return await _getPlannedExpensesListDataSource();
   }
 }
