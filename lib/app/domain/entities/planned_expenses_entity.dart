cd46f0841b3ccf720a1e0ce46da02e565e65f5a9
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-28 19:29:25 -0300

    fix: :bug: Alterando o tipo do dado

--- lib/app/domain/entities/planned_expenses_entity.dart
+++ lib/app/domain/entities/planned_expenses_entity.dart
@@ -3,7 +3,7 @@ import 'package:my_expenses/app/domain/entities/expense_entity.dart';
 class PlannedExpensesEntity {
   int? id;
   List<ExpenseEntity> expensesEntityList;
-  String month;
+  DateTime month;
   double? amount;
   double wage;
   double? remainder;
