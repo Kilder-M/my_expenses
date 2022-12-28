990ef37fc8247a9b777b61878b80646fca48ff4f
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-28 12:41:38 -0300

    feat: :sparkles: Listagem dos planned expenses

--- lib/app/presentation/modules/planned_expenses/views/planned_expenses_view.dart
+++ lib/app/presentation/modules/planned_expenses/views/planned_expenses_view.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:my_expenses/app/presentation/widgets/planned_expenses_card_widget.dart';
 
 import '../controllers/planned_expenses_controller.dart';
 
 class PlannedExpensesView extends GetView<PlannedExpensesController> {
+  const PlannedExpensesView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('PlannedExpensesView'),
-        centerTitle: true,
+      floatingActionButton: FloatingActionButton(
+        mini: true,
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
-      body: Center(
-        child: Text(
-          'PlannedExpensesView is working',
-          style: TextStyle(fontSize: 20),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      appBar: appBar(context),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(4, 8, 4, 0),
+        child: FutureBuilder(
+          future: controller.getPlannedExpensesLocalDataSource(),
+          initialData: const [],
+          builder: ((context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return Obx(()=>
+                 ListView.builder(
+                  itemCount: controller.plannedExpenseList.length,
+                  itemBuilder: ((context, index) {
+                    var plannedExpense = controller.plannedExpenseList[index];
+                    return PlannedExpensesCardWidget(title: plannedExpense.month);
+                  }),
+                ),
+              );
+            }
+          }),
         ),
       ),
+    );
+  }
+
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      title: const Text(
+        'Gastos Mensais',
+      ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.filter_list_outlined),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
