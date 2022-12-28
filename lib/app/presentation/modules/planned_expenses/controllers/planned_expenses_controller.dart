5ad2d9ee5e4cb2d800bfebe8c25f6e07b37c5a2a
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-28 12:40:54 -0300

    feat: :sparkles: Metodos de listar e salvar um planned expense

--- lib/app/presentation/modules/planned_expenses/controllers/planned_expenses_controller.dart
+++ lib/app/presentation/modules/planned_expenses/controllers/planned_expenses_controller.dart
@@ -1,20 +1,23 @@
 import 'package:get/get.dart';
+import 'package:my_expenses/app/domain/entities/planned_expenses_entity.dart';
+import 'package:my_expenses/app/domain/usecases/get_planned_expenses_usecase/get_planned_expenses_list_usecase.dart';
+import 'package:my_expenses/app/domain/usecases/save_planned_expenses_usecase/save_planned_expense_usecase.dart';
 
 class PlannedExpensesController extends GetxController {
-  //TODO: Implement PlannedExpensesController
+  final _savePlannedExpensesUsecase = Get.find<SavePlannedExpensesUseCase>();
+  final _getPlannedExpensesListUseCase =
+      Get.find<GetPlannedExpensesListUsecase>();
+  var plannedExpenseList = <PlannedExpensesEntity>[].obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  Future<bool> savePlannedExpenseLocalDataSource(
+      PlannedExpensesEntity plannedExpensesEntity) async {
+    return await _savePlannedExpensesUsecase(plannedExpensesEntity);
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<List<PlannedExpensesEntity>>
+      getPlannedExpensesLocalDataSource() async {
+    return plannedExpenseList.value = await _getPlannedExpensesListUseCase();
   }
 
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+
 }
