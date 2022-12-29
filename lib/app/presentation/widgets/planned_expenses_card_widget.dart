9e3f47fa2af997a5a95b10253a8192dff3400991
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-12-28 22:54:20 -0300

    feat: :lipstick: Adicionando card e outras alterações visuais

--- lib/app/presentation/widgets/planned_expenses_card_widget.dart
+++ lib/app/presentation/widgets/planned_expenses_card_widget.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
 
 class PlannedExpensesCardWidget extends StatelessWidget {
-  final String title;
-  const PlannedExpensesCardWidget({super.key, required this.title});
+  final String title, status;
+  final IconData statusIcon;
+  final Color iconColor;
+  final void Function()? onTap;
+  const PlannedExpensesCardWidget(
+      {super.key,
+      required this.title,
+      this.onTap,
+      required this.status,
+      required this.statusIcon,
+      required this.iconColor});
 
   @override
   Widget build(BuildContext context) {
-    return  ListTile(
-      // subtitle: Text('R\$: 1000,00'),
-      leading: const Icon(
-        Icons.calendar_month_outlined,
+    return Card(
+      elevation: 0.5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25),
       ),
-      trailing: const Icon(Icons.more_horiz),
-      title: Text(title),
+      shadowColor: Colors.deepPurple,
+      margin: const EdgeInsets.all(2),
+      child: ListTile(
+        subtitle: Row(
+          children: [
+            Icon(
+              statusIcon,
+              color: iconColor,
+              size: 16,
+            ),
+            Text(status),
+          ],
+        ),
+        leading: const CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Icon(
+            Icons.calendar_month_outlined,
+            color: Colors.black87,
+            size: 30,
+          ),
+        ),
+        onTap: onTap,
+        minLeadingWidth: 20,
+        trailing: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Icon(
+              Icons.more_horiz,
+              color: Colors.black87,
+            ),
+          ],
+        ),
+        title: Text(title),
+      ),
     );
   }
 }
