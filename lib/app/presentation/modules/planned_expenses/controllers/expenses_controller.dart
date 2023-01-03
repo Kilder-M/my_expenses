c528184af32cd93eb29f6666cc66bb393c70a0a2
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-02 23:45:14 -0300

    feat: :sparkles: me´todo para pegar a lista de gastos por id

--- lib/app/presentation/modules/planned_expenses/controllers/expenses_controller.dart
+++ lib/app/presentation/modules/planned_expenses/controllers/expenses_controller.dart
@@ -1,5 +1,13 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
+import 'package:my_expenses/app/domain/entities/expense_entity.dart';
+import 'package:my_expenses/app/domain/usecases/get_expenses_list_usecase/get_expenses_list_usecase.dart';
 
 class ExpensesController extends GetxController{
-  
+  final _getExpenseListbyId = Get.find<GetExpensesListByIdUsecase>();
+
+  var expensesList = <ExpenseEntity>[].obs;
+
+  Future<List<ExpenseEntity>> getExpenseListById(int id)async{
+    return expensesList.value = await _getExpenseListbyId(id);
+  }
 }
