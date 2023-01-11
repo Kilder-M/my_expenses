09975791c8c125b2660aa4ef0c638bbba49f2112
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-11 11:40:54 -0300

    refactor: :recycle: TextEditingControllers e formatters para o controlador da view

--- lib/app/presentation/modules/planned_expenses/controllers/planned_expenses_form_controller.dart
+++ lib/app/presentation/modules/planned_expenses/controllers/planned_expenses_form_controller.dart
@@ -1,14 +1,26 @@
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_expenses/app/domain/entities/planned_expenses_entity.dart';
 import 'package:my_expenses/app/domain/usecases/save_planned_expenses_usecase/save_planned_expense_usecase.dart';
+import 'package:my_expenses/app/presentation/modules/planned_expenses/controllers/planned_expenses_controller.dart';
 
 class PlannedExpensesFormController extends GetxController {
   final _savePlannedExpensesUsecase = Get.find<SavePlannedExpensesUseCase>();
+  final _plannedExpensesController = Get.find<PlannedExpensesController>();
+  final dateTextFormFieldController = TextEditingController();
+  final valueTextFormFieldController = TextEditingController();
+  final currecyFormat = CurrencyTextInputFormatter(
+    locale: 'pt',
+    symbol: 'R\$',
+  );
 
-  PlannedExpensesEntity plannedExpensesEntity = PlannedExpensesEntity(wage: 0,month: DateTime.now());
+  PlannedExpensesEntity plannedExpensesEntity =
+      PlannedExpensesEntity(wage: 0, month: DateTime.now());
 
-  Future<bool> savePlannedExpenseLocalDataSource(
+  savePlannedExpenseLocalDataSource(
       PlannedExpensesEntity plannedExpensesEntity) async {
-    return await _savePlannedExpensesUsecase(plannedExpensesEntity);
+    await _savePlannedExpensesUsecase(plannedExpensesEntity);
+    _plannedExpensesController.getPlannedExpensesLocalDataSource();
   }
 }
