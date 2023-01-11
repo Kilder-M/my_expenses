6d975193eb15cfd53dd7377b9f47901a396ec7aa
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-10 22:23:54 -0300

    refactor: :recycle: Renomeando método

--- lib/app/core/utils/show_date_picker_util.dart
+++ lib/app/core/utils/show_date_picker_util.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_expenses/app/core/utils/date_time_manager_util.dart';
 
 class ShowDatePickerUtil {
-  static Future<DateTime?> getFormattedDateTime(BuildContext context) async {
+  static Future<DateTime?> getDateTime(BuildContext context) async {
     return await showDatePicker(
       context: context,
       locale: const Locale('pt'),
