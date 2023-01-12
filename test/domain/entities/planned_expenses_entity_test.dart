9cff37a45d590755cacdc5b0a1a64a4548c7afef
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-12 17:01:03 -0300

    feat: :sparkles: Mudando regra de negócio

--- test/domain/entities/planned_expenses_entity_test.dart
+++ test/domain/entities/planned_expenses_entity_test.dart
@@ -1,28 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_expenses/app/domain/entities/expense_entity.dart';
 import 'package:my_expenses/app/domain/entities/planned_expenses_entity.dart';
 
 void main() {
   group('Testing planned expenses entity:', () {
-    ExpenseEntity expenseEntity1 = ExpenseEntity(
-      id: 1,
-      name: 'Agua',
-      paymentType: 'DEB',
-      status: 'Em aberto',
-      value: 90.0,
-      plannedExpensesId: 1,
-    );
-    ExpenseEntity expenseEntity2 = ExpenseEntity(
-      id: 1,
-      name: 'Luz',
-      paymentType: 'DEB',
-      status: 'Pago',
-      value: 190.0,
-      plannedExpensesId: 1,
-    );
     PlannedExpensesEntity plannedExpensesEntity = PlannedExpensesEntity(
       id: 1,
-      expensesEntityList: [expenseEntity1, expenseEntity2],
       month: DateTime(2022, 12),
       amount: 0.0,
       wage: 1200.0,
