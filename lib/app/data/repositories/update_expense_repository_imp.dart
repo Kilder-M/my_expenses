cf7316aa6c0678421c4ef4bf588c5c0e55535ff8
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-18 23:29:16 -0300

    fix: :bug: Invetendo dependencia correta

--- lib/app/data/repositories/update_expense_repository_imp.dart
+++ lib/app/data/repositories/update_expense_repository_imp.dart
@@ -1,9 +1,9 @@
-import 'package:my_expenses/app/data/data_sources/local/update_expense_local_data_source_imp.dart';
+import 'package:my_expenses/app/data/data_sources/update_expense_data_source.dart';
 import 'package:my_expenses/app/domain/entities/expense_entity.dart';
 import 'package:my_expenses/app/domain/repositories/update_expense_repository.dart';
 
 class UpdateExpenseRepositoryImp implements UpdateExpenseRepository {
-  final UpdateExpenseLocalDataSourceImp _updateExpenseLocalDataSource;
+  final UpdateExpenseDataSource _updateExpenseLocalDataSource;
 
   UpdateExpenseRepositoryImp(this._updateExpenseLocalDataSource);
   @override
