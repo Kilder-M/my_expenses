199ad994a70a1b15e4ad58a42ab2a63e321e0425
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-19 00:12:58 -0300

    feat: :sparkles: Método de atualizar um gasto

--- lib/app/presentation/modules/planned_expenses/controllers/expenses_controller.dart
+++ lib/app/presentation/modules/planned_expenses/controllers/expenses_controller.dart
@@ -1,13 +1,21 @@
 import 'package:get/get.dart';
 import 'package:my_expenses/app/domain/entities/expense_entity.dart';
 import 'package:my_expenses/app/domain/usecases/get_expenses_list_usecase/get_expenses_list_usecase.dart';
+import 'package:my_expenses/app/domain/usecases/update_expense_usecase/update_expense_usecase.dart';
 
 class ExpensesController extends GetxController{
   final _getExpenseListbyId = Get.find<GetExpensesListByIdUsecase>();
+  final _updateExpense = Get.find<UpdateExpenseUsecase>();
 
   var expensesList = <ExpenseEntity>[].obs;
 
   Future<List<ExpenseEntity>> getExpenseListById(int id)async{
     return expensesList.value = await _getExpenseListbyId(id);
+  }
+
+  Future<bool> updateExpenseStatusToPayedLocalDataSource(ExpenseEntity expenseEntity)async{
+    expenseEntity.isPayed = !expenseEntity.isPayed;
+    var response = await _updateExpense(expenseEntity);
+    return response;
   }
 }
