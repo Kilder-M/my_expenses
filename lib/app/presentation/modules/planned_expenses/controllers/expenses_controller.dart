e73cc0cf998b0bc70a9ccdca21c89f031284134d
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-20 12:03:29 -0300

    feat: :sparkles: deletar,atualizar gastos

--- lib/app/presentation/modules/planned_expenses/controllers/expenses_controller.dart
+++ lib/app/presentation/modules/planned_expenses/controllers/expenses_controller.dart
@@ -1,21 +1,35 @@
 import 'package:get/get.dart';
 import 'package:my_expenses/app/domain/entities/expense_entity.dart';
+import 'package:my_expenses/app/domain/usecases/delete_expense_usecase/delete_expense_usecase.dart';
 import 'package:my_expenses/app/domain/usecases/get_expenses_list_usecase/get_expenses_list_usecase.dart';
 import 'package:my_expenses/app/domain/usecases/update_expense_usecase/update_expense_usecase.dart';
 
-class ExpensesController extends GetxController{
+class ExpensesController extends GetxController {
   final _getExpenseListbyId = Get.find<GetExpensesListByIdUsecase>();
   final _updateExpense = Get.find<UpdateExpenseUsecase>();
+  final _deleteExpense = Get.find<DeleteExpenseUsecase>();
 
   var expensesList = <ExpenseEntity>[].obs;
+  var payedExpenseLenght = 0.obs;
 
-  Future<List<ExpenseEntity>> getExpenseListById(int id)async{
-    return expensesList.value = await _getExpenseListbyId(id);
+  getExpenseListById(int id) async {
+    var response = expensesList.value = await _getExpenseListbyId(id);
+    getPayedExpenseLenght();
+    return response;
   }
 
-  Future<bool> updateExpenseStatusToPayedLocalDataSource(ExpenseEntity expenseEntity)async{
-    expenseEntity.isPayed = !expenseEntity.isPayed;
-    var response = await _updateExpense(expenseEntity);
-    return response;
+  deleteExpenseUseCase(ExpenseEntity expenseEntity)async{
+    return await _deleteExpense(expenseEntity);
+  }
+
+  updateExpenseStatusLocalDataSource(ExpenseEntity expenseEntity) async {
+    return await _updateExpense(expenseEntity);
+  }
+
+  int getPayedExpenseLenght() {
+    return payedExpenseLenght.value = expensesList
+        .where((element) => element.isPayed == true)
+        .toList()
+        .length;
   }
 }
