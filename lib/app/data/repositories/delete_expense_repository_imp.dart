2f1f82cefaa0adedc90355bd2ed1ea2e703632b3
Kilder Montinegro <kilder.vsp@hotmail.com>
2023-01-20 11:55:05 -0300

    fix: :bug: Invertendo dependencia da classe correta

--- lib/app/data/repositories/delete_expense_repository_imp.dart
+++ lib/app/data/repositories/delete_expense_repository_imp.dart
@@ -1,13 +1,13 @@
-import 'package:my_expenses/app/data/data_sources/local/delete_expense_local_data_source_imp.dart';
+import 'package:my_expenses/app/data/data_sources/delete_expense_data_source.dart';
 import 'package:my_expenses/app/domain/entities/expense_entity.dart';
 import 'package:my_expenses/app/domain/repositories/delete_expense_repository.dart';
 
 class DeleteExpenseRepositoryImp implements DeleteExpenseRepository{
-  final DeleteExpenseLocalDataSourceImp _deleteExpenseLocalDataSource;
+  final DeleteExpenseDataSource _deleteExpenseDataSource;
 
-  DeleteExpenseRepositoryImp(this._deleteExpenseLocalDataSource);
+  DeleteExpenseRepositoryImp(this._deleteExpenseDataSource);
   @override
   Future<bool> call(ExpenseEntity expenseEntity) async{
-    return await _deleteExpenseLocalDataSource(expenseEntity);
+    return await _deleteExpenseDataSource(expenseEntity);
   }
 }
