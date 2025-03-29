9358051cf7d538002559e7c683da04290cb97126
Kilder <kilder.vsp@hotmail.com>
2025-03-29 17:19:40 -0300

    removing useless  import

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:my_expenses/app/core/utils/l10n/l10n.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:my_expenses/app/presentation/widgets/myexpensive_theme_data.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
