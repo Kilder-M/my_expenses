bd09a548ae10ef4d72f6dd7899a5dc26690b0162
Kilder <kilder.vsp@hotmail.com>
2025-04-07 23:54:44 -0300

    Fixing calendar initialDate and null excepetion when a date is not selected.

--- lib/app/core/utils/date_time_manager_util.dart
+++ lib/app/core/utils/date_time_manager_util.dart
@@ -3,6 +3,6 @@ import 'package:intl/intl.dart';
 class DateTimeManagerUtil {
 
   static String getYearAndMonth(DateTime dateTime){
-    return DateFormat('${DateFormat.MONTH}/${DateFormat.YEAR}','pt').format(dateTime).toString();
+    return DateFormat('${DateFormat.MONTH}/${DateFormat.YEAR}',).format(dateTime).toString();
   }
 }
